a675aa2127c23ba14bec6db9531d952949932a3e
Tenevir <dan.tenevir@gmail.com>
2025-08-10 11:04:18 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.update({name: self})
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     persons_list = [Person(person["name"], person["age"]) for person in people]
     add_marital_relations(people)
     return persons_list
@@ -15,8 +16,8 @@ def create_person_list(people: list) -> list:
 
 def add_marital_relations(people: list) -> None:
     for person in people:
-        if person.get("wife") is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif person.get("husband") is not None:
+        elif person.get("husband"):
             Person.people[person["name"]].husband =\
                 Person.people[person["husband"]]
